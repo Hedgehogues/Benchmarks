9e6b933775540114cbe4c37b1879a68ec0ad4f89
Egor <hedgehogues@bk.ru>
2024-09-18 20:12:06 +0700

    Update vanila_psql.sql

--- code/ sharding/vanila_psql.sql
+++ code/ sharding/vanila_psql.sql
@@ -1,17 +1,17 @@
--- Подключаем необходимые расширения
+-- Enable the necessary extensions
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 
--- Создаем серверы для каждого шарда
+-- Create servers for each shard
 CREATE SERVER shard1 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'shard1_host', dbname 'your_db', port '5432');
 CREATE SERVER shard2 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'shard2_host', dbname 'your_db', port '5432');
 CREATE SERVER shard3 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'shard3_host', dbname 'your_db', port '5432');
 
--- Создаем маппинг пользователей
+-- Create user mappings for each shard
 CREATE USER MAPPING FOR postgres SERVER shard1 OPTIONS (user 'your_user', password 'your_password');
 CREATE USER MAPPING FOR postgres SERVER shard2 OPTIONS (user 'your_user', password 'your_password');
 CREATE USER MAPPING FOR postgres SERVER shard3 OPTIONS (user 'your_user', password 'your_password');
 
--- Создаем таблицы на каждом шарде
+-- Create foreign tables on each shard
 CREATE FOREIGN TABLE news_shard1 (
     id SERIAL PRIMARY KEY,
     category_id INT,
@@ -33,7 +33,7 @@ CREATE FOREIGN TABLE news_shard3 (
     content TEXT
 ) SERVER shard3 OPTIONS (table_name 'news');
 
--- Создаем основную таблицу через VIEW
+-- Create a main table using a VIEW to combine data from all shards
 CREATE VIEW news AS
 SELECT * FROM news_shard1
 UNION ALL
@@ -41,7 +41,7 @@ SELECT * FROM news_shard2
 UNION ALL
 SELECT * FROM news_shard3;
 
--- Вставка данных
+-- Insert data into the news view (data will be distributed across shards)
 INSERT INTO news (category_id, title, content) VALUES (1, 'Title 1', 'Content 1');
 INSERT INTO news (category_id, title, content) VALUES (2, 'Title 2', 'Content 2');
 INSERT INTO news (category_id, title, content) VALUES (3, 'Title 3', 'Content 3');
